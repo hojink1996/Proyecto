4109d9a9b0f50b8edb3d2cd7911fcaa97f8a2965
Hojin Kang <hojink1996@gmail.com>
2018-10-24 20:16:27 -0300

    Agregar conjunto de tags

--- load_images.py
+++ load_images.py
@@ -1,9 +1,10 @@
 import glob
 import cv2
 import matplotlib.pyplot as plt
+import os
 
 # Location of file with tags and images
-tags = glob.glob('/home/hojin/Documentos/Primavera 2018/Inteligencia/Proyecto/tiny-imagenet-200/words.txt')
+tags = '/home/hojin/Documentos/Primavera 2018/Inteligencia/Proyecto/tiny-imagenet-200/words.txt'
 images = glob.glob('/home/hojin/Documentos/Primavera 2018/Inteligencia/Proyecto/tiny-imagenet-200/train/**/*.JPEG',
                    recursive = True)
 
@@ -11,13 +12,33 @@ images = glob.glob('/home/hojin/Documentos/Primavera 2018/Inteligencia/Proyecto/
 with open(tags) as tag:
     content = tag.readlines()
 
+# We make a HashMap (dictionary) with the identifiers
+identificadores = {}
+
+# Link every directory with it's word
+for palabra in content:
+    linea = palabra.split('\t')
+    codigo = linea[0]
+    descriptor = linea[1].strip('\n')
+    identificadores[codigo] = descriptor
+
+# Set of the words relating image with it's identifier
+image_word = []
+
 # Save images
 image_set = []
 for img in images:
     image = cv2.imread(img)
     image_set.append(image)
+    corresponding_link = os.path.basename(img)
+    corresponding_link = corresponding_link.split('_')[0]
+    image_word.append(identificadores[corresponding_link])
+
+# Image to visualize
+n = 100
 
 # Visualize an image
-imagen =image_set[100]
+imagen =image_set[n]
 imgplot = plt.imshow(imagen)
 plt.show()
+print(image_word[n])
