af7ee8f6a7ec22d9b9a201e5a69f88f184ca2fca
Hojin Kang <hojink1996@gmail.com>
2018-11-18 22:11:10 -0300

    Add batch generation

--- adv_example_generation.py
+++ adv_example_generation.py
@@ -43,6 +43,41 @@ def fast_gradient(model, x, eps=0.25):
 
     return xadv, signo
 
+def fast_gradient_batch_generation(model, x, eps=0.25):
+    """
+    Generates an adversarial example for the model using the fast gradient method
+
+    :param      model   : The model from which to generate an adversarial example
+    :param      x       : An array of images from which to generate the adversarial examples
+    :param      eps     : The epsilon parameter that ponderates the gradient sign
+
+    :return:    A tuple containing the adversarial examples generated and the filters used to generate them
+    """
+    # Predicted result in normal case
+    y = model.predict(x).argmax()
+
+    # Make predicted variable into categorical variable (0s or 1s) of 1000 classes (ImageNet)
+    y_categorical = to_categorical(y, 1000)
+
+    # Make the predicted class the target
+    esperado = K.variable(y_categorical)
+
+    # Set the loss function as Cross Entropy (for Classification problem)
+    costo = metrics.categorical_crossentropy(model.output, esperado)
+
+    # Get the gradient of the function
+    gradiente = K.gradients(costo, model.input)
+    val_gradiente = K.function([model.input], gradiente)
+
+    xadv = []
+    filter = []
+    for x_image in x:
+        # Remember that the adversarial examples are x + eps*sign(gradient)
+        signo = np.sign(val_gradiente([x_image])[0])
+        xadv.append(x_image + eps*signo)
+        filter.append(signo)
+
+    return xadv, filter
 
 def deepfool(x, model, eps=1e-6, max_iter=100, classes=1000, search_classes=31):
     """
